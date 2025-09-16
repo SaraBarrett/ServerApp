14b7cda1618d47aff9945e71bf20e78a6bea9587
SaraBarrett <sara.c.mon@gmail.com>
2025-09-16 14:10:55 +0100

    php start

--- index.php
+++ index.php
@@ -12,5 +12,9 @@
     <button id="cat-fact">Cat Fact</button>
     <button id="dog-image">Dog Image</button>
     <button id="weather-btn">☁️</button>
+    <h4>Sistema de navegação na minha app</h4>
+    <ul>
+      <li><a href="phpCode.php">Página de PHP</a></li>
+    </ul>
   </body>
 </html>
